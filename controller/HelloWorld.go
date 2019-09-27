01e9d0dcd90c629d0098d0c78c478aa1575ca4ee
Yanlong-li <ahlyl94@gmail.com>
2019-09-27 18:28:37 +0800

    debug 切片

--- controller/HelloWorld.go
+++ controller/HelloWorld.go
@@ -11,16 +11,42 @@ func init() {
 	route.Register(packet.HelloWorld{}, HelloWorld)
 	route.Register(packet.Token{}, LoginSuccess)
 	route.Register(packet.LoginFail{}, LoginFail)
+	route.Register(packet.UserList{}, UserList)
 }
 
 func HelloWorld(world packet.HelloWorld, conn *connect.Connector) {
-
-	conn.Send(packet.Login{Username: "张三", Password: "123456"})
+	fmt.Println("请输入姓名")
+	var name string
+	//_, err := fmt.Scanln(&name)
+	//if err != nil {
+	//	log.Fatal("输入失败")
+	//}
+	name = "张三"
+	conn.Send(packet.Login{Username: name, Password: "123456"})
 }
 
 func LoginSuccess(token packet.Token, conn *connect.Connector) {
 	fmt.Println("登录成功：", token.Token)
+	//for {
+	fmt.Println("请求用户列表")
+	conn.Send(packet.GetUserList{})
+	//}
+
 }
 func LoginFail(fail packet.LoginFail, conn *connect.Connector) {
 	fmt.Println("登录失败：", fail.Message)
+	fmt.Println("请输入姓名")
+	var name string
+	//_, err := fmt.Scanln(&name)
+	//if err != nil {
+	//	log.Fatal("输入失败")
+	//}
+	name = "张三"
+	conn.Send(packet.Login{Username: name, Password: "123456"})
+}
+
+func UserList(list packet.UserList, conn *connect.Connector) {
+
+	fmt.Println(list)
+
 }
