147094ea93b0c062fa8245baf54431b81d1f0ac4
Yanlong-li <ahlyl94@gmail.com>
2020-03-06 15:50:43 +0800

    增加心跳 一分钟一次

--- controller/Connected.go
+++ controller/Connected.go
@@ -5,6 +5,7 @@ import (
 	"github.com/yanlong-li/HelloWorld-GO/io/network/connect"
 	"github.com/yanlong-li/HelloWorld-GO/io/network/route"
 	"github.com/yanlong-li/HelloWorldServer/packetModel"
+	"github.com/yanlong-li/HelloWorldServer/packetModel/gateway"
 )
 
 func init() {
@@ -13,4 +14,6 @@ func init() {
 
 func Connected(connector connect.Connector) {
 	fmt.Println("新连接", connector.GetId())
+	// 发送心跳
+	connector.Send(gateway.Heartbeat{Sn: 0})
 }
--- controller/gateway/HeartbeatController.go
+++ controller/gateway/HeartbeatController.go
@@ -4,6 +4,7 @@ import (
 	"github.com/yanlong-li/HelloWorld-GO/io/network/connect"
 	"github.com/yanlong-li/HelloWorld-GO/io/network/route"
 	"github.com/yanlong-li/HelloWorldServer/packetModel/gateway"
+	"time"
 )
 
 func init() {
@@ -12,5 +13,6 @@ func init() {
 
 func Heartbeat(heartbeat gateway.Heartbeat, conn connect.Connector) {
 
+	time.Sleep(time.Minute)
 	conn.Send(gateway.Heartbeat{Sn: heartbeat.Sn + 1})
 }
