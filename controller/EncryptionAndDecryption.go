41e595182553c7004da0ba93efcebd7a541633f4
Yanlong-li <ahlyl94@gmail.com>
2020-03-08 00:08:41 +0800

    模拟加密解密动作

--- controller/EncryptionAndDecryption.go
+++ controller/EncryptionAndDecryption.go
@@ -1,20 +1,35 @@
 package controller
 
 import (
-	"fmt"
+	"github.com/yanlong-li/HelloWorld-GO/io/logger"
 	"github.com/yanlong-li/HelloWorld-GO/io/network/connect"
 	"github.com/yanlong-li/HelloWorld-GO/io/network/route"
-	"github.com/yanlong-li/HelloWorldServer/packetModel/encryp"
+	"github.com/yanlong-li/HelloWorld-GO/io/network/stream"
+	"github.com/yanlong-li/HelloWorldServer/packetModel"
+	"github.com/yanlong-li/HelloWorldServer/packetModel/encrypt"
 )
 
 //加密和解密
 
 func init() {
-	route.Register(encryp.EncryptData{}, Encryption)
+	route.Register(packetModel.BeforeSending{}, Encryption)
+	route.Register(encrypt.BytesData{}, Decryption)
 }
 
 // 加密动作
-func Encryption(data encryp.EncryptData, connector connect.Connector) {
-	fmt.Println(data)
-	connector.HandleData(data.Data)
+func Encryption(ps stream.Interface) []byte {
+
+	//简单做了一层封装
+	model := encrypt.BytesData{}
+	model.Data = ps.ToData()
+
+	ps.Marshal(model)
+
+	return ps.ToData()
+}
+func Decryption(encryptData encrypt.BytesData, conn connect.Connector) {
+
+	logger.Debug("收到一个加密数据", 0)
+	conn.HandleData(encryptData.Data)
+
 }
