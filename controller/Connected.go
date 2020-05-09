0cd703e4cb9ed78321c59b518e7daa817b48bb51
Yanlong-li <ahlyl94@gmail.com>
2020-05-09 16:43:49 +0800

    取消加密动作 增加登录测试 增加获取朋友列表 增加获取黑名单列表 增加获取请求列表

--- controller/Connected.go
+++ controller/Connected.go
@@ -2,18 +2,51 @@ package controller
 
 import (
 	"fmt"
+	"github.com/yanlong-li/HelloWorld-GO/io/logger"
 	"github.com/yanlong-li/HelloWorld-GO/io/network/connect"
 	"github.com/yanlong-li/HelloWorld-GO/io/network/route"
 	"github.com/yanlong-li/HelloWorldServer/packetModel"
 	"github.com/yanlong-li/HelloWorldServer/packetModel/gateway"
+	"github.com/yanlong-li/HelloWorldServer/packetModel/user/Login"
+	"github.com/yanlong-li/HelloWorldServer/packetModel/user/contacts"
 )
 
 func init() {
 	route.Register(packetModel.Connected{}, Connected)
+	route.Register(Login.Success{}, loginSuccess)
+	route.Register(contacts.List{}, func(list contacts.List, conn connect.Connector) {
+		logger.Debug("获取朋友列表成功", 0, list.List)
+	})
+	route.Register(contacts.RequestList{}, func(list contacts.RequestList, conn connect.Connector) {
+		logger.Debug("获取新朋友列表成功", 0, list.List)
+	})
+	route.Register(contacts.Blacklist{}, func(list contacts.Blacklist, conn connect.Connector) {
+		logger.Debug("获取黑名单列表成功", 0, list.List)
+	})
+}
+
+func loginSuccess(success Login.Success, conn connect.Connector) {
+
+	logger.Debug("登陆成功", 0, success.Token)
+
+	// 获取联系人列表
+	conn.Send(contacts.GetList{})
+	//获取新朋友列表
+	conn.Send(contacts.GetRequestList{})
+	//获取黑名单列表
+	conn.Send(contacts.GetBlacklist{})
+}
+
+func LoginFail(fail Login.Fail, conn connect.Connector) {
+
+	logger.Debug("登陆失败", 0)
 }
 
 func Connected(connector connect.Connector) {
 	fmt.Println("新连接", connector.GetId())
 	// 发送心跳
 	_ = connector.Send(gateway.Heartbeat{Sn: 0})
+
+	// 发送认证请求
+	_ = connector.Send(Login.ForEmail{Email: "895185921@qq.com", Password: "123456"})
 }
--- controller/EncryptionAndDecryption.go
+++ controller/EncryptionAndDecryption.go
@@ -1,22 +1,19 @@
 package controller
 
 import (
-	"github.com/ProtonMail/gopenpgp/v2/helper"
 	"github.com/yanlong-li/HelloWorld-GO/io/logger"
 	"github.com/yanlong-li/HelloWorld-GO/io/network/connect"
 	"github.com/yanlong-li/HelloWorld-GO/io/network/packet"
-	"github.com/yanlong-li/HelloWorld-GO/io/network/route"
 	"github.com/yanlong-li/HelloWorld-GO/io/network/stream"
-	"github.com/yanlong-li/HelloWorldServer/packetModel"
 	"github.com/yanlong-li/HelloWorldServer/packetModel/encrypt"
-	"io/ioutil"
 )
 
 //加密和解密
 
 func init() {
-	route.Register(packetModel.BeforeSending{}, Encryption)
-	route.Register(encrypt.BytesData{}, Decryption)
+
+	//route.Register(packetModel.BeforeSending{}, Encryption)
+	//route.Register(encrypt.BytesData{}, Decryption)
 }
 
 // 加密动作
@@ -34,23 +31,14 @@ func Decryption(encryptData encrypt.BytesData, conn connect.Connector) {
 
 	logger.Debug("收到一个加密数据", 0)
 
-	privateKeyBytes, err := ioutil.ReadFile("config/8FF1A48D9B97C67F9B8C4F633708AAA986797D5D.asc")
-
-	if err != nil {
-		logger.Warning("读取ecdsa密钥失败", 0, err)
-	}
-	c, e := helper.DecryptMessageArmored(string(privateKeyBytes), []byte("123456"), string(encryptData.Data))
-	if e != nil {
-		logger.Warning("解密失败", 0, e)
-	}
-	buf := []byte(c)
-	if len(buf) < packet.OpCodeLen+packet.BufLenLen {
-		logger.Warning("解密后的数据无效", 0, buf)
+	if len(encryptData.Data) < packet.OpCodeLen+packet.BufLenLen {
+		logger.Warning("解密后的数据无效", 0, encryptData.Data)
+		return
 	}
 	//自行处理粘包和拆包，单个包处理后丢给 conn.HandleData()
 
 	//bufLen := buf[:packet.BufLenLen]
-	bufData := buf[packet.BufLenLen:]
+	bufData := encryptData.Data[packet.BufLenLen:]
 	conn.HandleData(bufData)
 
 }
